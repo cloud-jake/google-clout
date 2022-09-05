7c6c9115755fca6fe74676f670e824b393cd06b4
cloud-jake <jake.holmquist@kinandcarta.com>
2022-09-05 12:05:43 -0400

    vallidated steps 00 & 01

--- clout-9-workflow/00.sh
+++ clout-9-workflow/00.sh
@@ -72,14 +72,3 @@ gcloud projects add-iam-policy-binding $PROJECT \
 
 gcloud run services update ${SERVICE_NAME} --service-account ${SERVICE_ACCOUNT}@${PROJECT}.iam.gserviceaccount.com --region=${REGION}
 
-
-
-# 4 - Trigger the workflow with an authenticated POST request to the Cloud Run service.
-#     You can use the curl command below to achieve this. Substitute the Cloud Run endpoint URL from step 2 into the environment variable and run the curl command.
-
-
-#  export CLOUD_RUN_ENDPOINT=[Cloud Run Endpoint URL]
-
-
-curl -H "Authorization: Bearer $(gcloud auth print-identity-token)" \
-    -X POST $CLOUD_RUN_ENDPOINT
